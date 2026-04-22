3efdb2d8f3100a0dcb0c3f09d305b8b3736d5563
Meghashyam <shyamroyal916kdm@gmail.com>
2026-04-22 09:35:16 +0530

    Update breadcrumb.tsx

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -4,7 +4,7 @@ import { ChevronRight, MoreHorizontal } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
-function Breadcrumb({ ...props }: React.ComponentProps<'nav'>) {
+function Breadcrumb({ ...props }: React.ComponentProps<'nav'>) { 
   return <nav aria-label="breadcrumb" data-slot="breadcrumb" {...props} />
 }
 
