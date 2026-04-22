cee4e1337afa3060152a5a727020a427bd87c3eb
Meghashyam <shyamroyal916kdm@gmail.com>
2026-04-22 09:39:16 +0530

    Update menubar.tsx

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -11,7 +11,7 @@ function Menubar({
   ...props
 }: React.ComponentProps<typeof MenubarPrimitive.Root>) {
   return (
-    <MenubarPrimitive.Root
+    <MenubarPrimitive.Root 
       data-slot="menubar"
       className={cn(
         'bg-background flex h-9 items-center gap-1 rounded-md border p-1 shadow-xs',
