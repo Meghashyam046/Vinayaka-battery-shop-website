3cb33610405e50016cf81e605426ba3b66bae461
Meghashyam <shyamroyal916kdm@gmail.com>
2026-04-22 09:39:53 +0530

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -6,7 +6,7 @@ import * as ProgressPrimitive from '@radix-ui/react-progress'
 import { cn } from '@/lib/utils'
 
 function Progress({
-  className,
+  className, 
   value,
   ...props
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
